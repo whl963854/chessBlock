97c4af1181632804e41a6d2cf998c034a706f4a2
Wang Hailong <3366576307@qq.com>
2022-11-07 15:57:24 +0800

    chessBlocks

---
+++ src/settings.082bd.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"4519a",main:"73621"}};
